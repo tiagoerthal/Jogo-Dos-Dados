3c5d333ea0ab4b771051cb7dc48a496cafa75870
tiago <tiagoeweber@gmail.com>
2025-04-01 19:16:19 -0300

    Add project files.

---
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoDosDados.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

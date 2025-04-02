34f00e7d0673164ec5d7e8af71571444d63022c8
tiago <tiagoeweber@gmail.com>
2025-04-02 20:56:48 -0300

    Correção de Erros no codígo

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -11,29 +11,59 @@ namespace JogoDosDados.ConsoleApp
             while(true)
             {            
                 int posicaoUsuario = 0;
+                int posicaoComputador = 0;
 
                 bool jogoEstaEmAndamento = true;
 
                 while(jogoEstaEmAndamento)
                 {
-                    ExibirCabecalho();
+                    //turno do usuario
+                    ExibirCabecalho("Jogador");
 
                     int resultado = LancarDado();
 
                     ExibirResultadoSorteio(resultado);
 
                     posicaoUsuario += resultado;
-
-                    Console.WriteLine($"O Jogador está na posíção: {posicaoUsuario} do {limiteLinhaChegado}");
+                    
 
                     if (posicaoUsuario >= limiteLinhaChegado)
                     {
                         Console.WriteLine("Parabéns, você alcançou a linha de chegada!");
+                        Console.ReadLine();
 
                         jogoEstaEmAndamento = false;
+                        continue;
                     }
                     else
-                        Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {limiteLinhaChegado}");
+                    Console.WriteLine($"O jogador está na posição: {posicaoUsuario} de {limiteLinhaChegado}");
+
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Pressione ENTER para Continuar....");
+                    Console.ReadLine();
+
+                 
+                    
+                    //turno do computador
+                    ExibirCabecalho("Computador");
+
+                    int resultadoDoComputador = LancarDado();
+
+                    ExibirResultadoSorteio(resultadoDoComputador);
+
+                    posicaoComputador += resultado;
+
+
+                    if (posicaoComputador >= limiteLinhaChegado)
+                    {
+                        Console.WriteLine("Que pena!, o computador alcançou a linha de chegada");
+                        Console.ReadLine();
+
+                        jogoEstaEmAndamento = false;
+                        continue;
+                    }
+                    else
+                        Console.WriteLine($"O Computador está na posição: {posicaoComputador} de {limiteLinhaChegado}");
 
                     Console.WriteLine("----------------------------------------");
                     Console.WriteLine("Pressione ENTER para Continuar....");
@@ -47,15 +77,20 @@ namespace JogoDosDados.ConsoleApp
             }           
         }
 
-        static void ExibirCabecalho()
+        static void ExibirCabecalho(string nomeJogador)
         {
             Console.Clear();
             Console.WriteLine("----------------------------------------");
             Console.WriteLine("Jogo Dos Dados");
+            Console.WriteLine($"Turno do: {nomeJogador}");
             Console.WriteLine("----------------------------------------");
 
-            Console.Write("Pressione ENTER para Lançar o dado...");
-            Console.ReadLine();
+            if (nomeJogador != "Computador")
+            {
+                Console.Write("Pressione ENTER para Lançar o dado...");
+                Console.ReadLine();
+            }
+          
         }
 
         static int LancarDado()
